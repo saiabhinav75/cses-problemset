7211e541b90219d8f53ce23d9678b95870d96044
abhinav <abhi.gandesree@eksaq.in>
2025-11-04 10:03:59 +0530

    concert tickets complete

--- sorting-and-searching/concert-tickets.cpp
+++ sorting-and-searching/concert-tickets.cpp
@@ -1,44 +1,29 @@
 #include "../stdc++.h"
 using namespace std;
 
-// incomplete
-int binSearch(vector<int> &tickets, vector<bool> &visit, int maxPay)
-{
-
-    int l = 0;
-    int h = tickets.size() - 1;
-    int ans = -1;
-    while (l <= h)
-    {
-        int mid = l + h / 2;
-        if (tickets[mid] <= maxPay)
-        {
-            ans = mid;
-            l = mid + 1;
-        }
-        else
-        {
-            h = mid - 1;
-        }
-    }
-    tickets[ans]=1e9;
-    return ans;
-}
-
 int main()
 {
     int n, m;
     cin >> n >> m;
-    vector<int> tickets(n);
-    vector<bool> ticketsTaken(n);
-    vector<int> maxpay(m);
+    set<pair<int,int> > sett;
     for (int i = 0; i < n; i++)
-        cin >> tickets[i];
-    sort(tickets.begin(), tickets.end());
+    {
+        int k;
+        cin >> k;
+        sett.insert({k,i});
+    }
     for (int i = 0; i < m; i++)
     {
-        cin >> maxpay[i];
-        cout << binSearch(tickets, ticketsTaken, maxpay[m]) << endl;
+        int cur;
+        cin >> cur;
+        auto match = sett.lower_bound({cur + 1,0});
+        if(match == sett.begin()){
+            cout<<-1<<endl;
+        } else{
+            match--;
+            cout<<(*match).first<<endl;
+            sett.erase(match);
+        }
     }
 }
 /*
